aa8f40d4815232a851c87d676b7de29c1d8c8581
Beatrice Hahn <beatricehahn7@gmail.com>
2023-08-03 20:54:52 -0700

    Add login  with token auth to frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,73 @@ const App = () => {
     )  
   }, [])
 
+  // login handler
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+    console.log('logging in with', username, password)
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <div>
+        username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  // const blogForm = () => {
+  //   <form onSubmit={addBlog}>
+  //     <input
+  //       value={newBlog}
+  //       onChange={handleBlogChange}
+  //     />
+  //     <button type='submit'>save</button>
+  //   </form>
+  // }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h1>Blogs list</h1>
+
+      {!user && loginForm()}
+      {user && <div>
+          <p>{user.name} logged in</p>
+        </div>
+      }
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,10 +1,33 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+// private token variable
+let TOKEN = null
+
+// chages token value with auth header
+const setToken = newToken => {
+  TOKEN = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+const create = async newObject => {
+  // 
+  const config = {
+    headers: { Authorization: TOKEN }
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${ baseUrl }/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { getAll, create, update, setToken }
