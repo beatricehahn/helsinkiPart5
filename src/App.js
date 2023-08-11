870b6a67c01766373468aff350b68ececad8ec62
Beatrice Hahn <beatricehahn7@gmail.com>
2023-08-11 11:54:33 -0700

    Add blog state

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import loginService from './services/login'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+
+  // blogs from db
   const [blogs, setBlogs] = useState([])
+
+  // states for login credentials
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  // states for creating a new blog
+  const [newBlog, setNewBlog] = useState({
+    url: '',
+    title: '',
+    author: ''
+  })
+
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs)
     )  
   }, [])
 
@@ -91,15 +103,60 @@ const App = () => {
     </form>
   )
 
-  // const blogForm = () => {
-  //   <form onSubmit={addBlog}>
-  //     <input
-  //       value={newBlog}
-  //       onChange={handleBlogChange}
-  //     />
-  //     <button type='submit'>save</button>
-  //   </form>
-  // }
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedUserJSON = window.localStorage.getItem('loggedUser')
+      const token = loggedUserJSON.token
+
+      blogService.setToken(token)
+
+      blogService.create(newBlog)
+
+      // reset blog
+      setNewBlog({
+        url: '',
+        title: '',
+        author: ''
+      })
+    } catch (exception) {
+        setErrorMessage('Error creating a new blog post')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+    }
+
+    console.log('submitted a new blog')
+  }
+
+  const handleUrlChange = (event) => {
+    setNewBlog({ ...newBlog, url: event.target.value})
+  }
+
+  const handleTitleChange = (event) => {
+    setNewBlog({ ...newBlog, title: event.target.value })
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewBlog({ ...newBlog, author: event.target.value })
+  }
+
+  const blogForm = () => (
+    <div>
+      <form onSubmit={handleCreate}>
+        <label>Url:</label>
+        <input type='text' value={newBlog.url} onChange={handleUrlChange}></input>
+
+        <label>Title:</label>
+        <input type='text' value={newBlog.title} onChange={handleTitleChange}></input>
+
+        <label>Author:</label>
+        <input type='text' value={newBlog.author} onChange={handleAuthorChange}></input>
+
+        <button type='submit'>Add blog</button>
+      </form>
+    </div>
+  )
 
   return (
     <div>
@@ -109,8 +166,10 @@ const App = () => {
       {user && <div>
           <p>{user.name} logged in</p>
           <button onClick={handleLogout}>Log out</button>
+          {blogForm()}
         </div>
       }
+  
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
