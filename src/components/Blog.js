b2beddedd3a196e56396853bd06fda255d598bca
Beatrice Hahn <beatricehahn7@gmail.com>
2023-08-18 14:11:15 -0700

    Toggle additional blog entry details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,19 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import Toggler from "./Toggler"
+import './styles/Blog.styles.css'
+
+const Blog = ( {blog} ) => (
+  <div className='blogWrapper'>
+    <div className='innerWrapper'>
+      {blog.title} by {blog.author}
+      <Toggler primaryLabel='view' secondaryLabel='hide'>
+        <div>
+          {blog.url}
+          <br/>
+          likes: {blog.likes} <button>like</button>
+          <br/>
+        </div>
+      </Toggler>
+    </div>
   </div>  
 )
 
